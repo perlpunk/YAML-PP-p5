a7c090b7d21f3971a529a06ba8a95f4863ef166c
Tina Müller <cpan2@tinita.de>
2018-09-12 01:11:28 +0200

    Refactor 10.parse-valid.t

--- t/lib/YAML/PP/Test.pm
+++ t/lib/YAML/PP/Test.pm
@@ -2,6 +2,19 @@ package YAML::PP::Test;
 use strict;
 use warnings;
 
+use File::Basename qw/ dirname basename /;
+use Encode;
+use Test::More;
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = bless {
+        stats => {},
+        %args,
+    }, $class;
+    return $self;
+}
+
 sub get_tags {
     my ($class, %args) = @_;
     my %id_tags;
@@ -62,5 +75,187 @@ sub get_tests {
 
     return @dirs;
 }
+
+sub read_tests {
+    my ($self, %args) = @_;
+    my $test_suite_dir = $self->{test_suite_dir};
+    my $dir = $self->{dir};
+    my $valid = $self->{valid};
+    my $skip = $args{skip};
+
+    my @dirs = $self->get_tests(
+        test_suite_dir => $test_suite_dir,
+        dir => $dir,
+        valid => $valid,
+        %args,
+    );
+
+    my @todo = ();
+
+    if ($ENV{TEST_ALL}) {
+        @todo = @$skip;
+        @$skip = ();
+    }
+
+    if (my $dir = $ENV{YAML_TEST_DIR}) {
+        @dirs = ($dir);
+        @todo = ();
+        @$skip = ();
+    }
+
+    my $skipped;
+    @$skipped{ @$skip } = (1) x @$skip;
+
+    my %todo;
+    @todo{ @todo } = ();
+
+    my @testcases;
+    for my $dir (sort @dirs) {
+        my $id = basename $dir;
+
+        open my $fh, '<', "$dir/===" or die $!;
+        chomp(my $title = <$fh>);
+        close $fh;
+
+        my @test_events;
+        if ($self->{events}) {
+            open my $fh, '<', "$dir/test.event" or die $!;
+            chomp(@test_events = <$fh>);
+            close $fh;
+        }
+
+        my $in_yaml;
+        if ($self->{in_yaml}) {
+            open my $fh, "<", "$dir/in.yaml" or die $!;
+            my $yaml = do { local $/; <$fh> };
+            close $fh;
+            $in_yaml = decode_utf8($yaml);
+        }
+
+        my $test = {
+            id => $id,
+            dir => dirname($dir),
+            title => $title,
+            test_events => \@test_events,
+            in_yaml => $in_yaml,
+        };
+        push @testcases, $test;
+    }
+
+    $self->{skipped} = $skipped;
+    $self->{todo} = \%todo;
+    $self->{testcases} = \@testcases;
+    return (\@testcases);
+}
+
+sub run_testcases {
+    my ($self, %args) = @_;
+    my $skipped = $self->{skipped};
+    my $testcases = $self->{testcases};
+    my $todos = $self->{todo};
+    my $code = $args{code};
+    my $skip_count = keys %$skipped;
+    my $results = $self->{stats};
+    @$results{qw/ DIFFS DIFF OK ERROR TODO SKIP /} = ([], (0) x 5);
+
+    for my $testcase (@$testcases) {
+        my $id = $testcase->{id};
+        my $skip = delete $skipped->{ $id };
+#        next if $skip;
+        my $todo = exists $todos->{ $id };
+        $testcase->{todo} = $todo;
+
+    #    diag "------------------------------ $id";
+
+        my $result;
+        if ($skip) {
+            SKIP: {
+                $results->{SKIP}++;
+                skip "SKIP $id", 1 if $skip;
+                $result = $code->($self, $testcase);
+            }
+        }
+        elsif ($todo) {
+            TODO: {
+                local $TODO = $todo;
+                $result = $code->($self, $testcase);
+            }
+        }
+        else {
+            $result = $code->($self, $testcase);
+        }
+
+    }
+    if (keys %$skipped) {
+        # are there any leftover skips?
+        warn __PACKAGE__.':'.__LINE__.$".Data::Dumper->Dump([\$skipped], ['skipped']);
+    }
+#    diag "Skipped $skip_count tests";
+
+}
+
+sub parse_events {
+    my ($class, $testcase) = @_;
+
+    my @events;
+    my $parser = YAML::PP::Parser->new(
+        receiver => sub {
+            my ($self, @args) = @_;
+            push @events, YAML::PP::Parser->event_to_test_suite(\@args);
+        },
+    );
+    eval {
+        $parser->parse_string($testcase->{in_yaml});
+    };
+    my $err = $@;
+    my $line = $parser->lexer->line;
+    return {
+        events => \@events,
+        err => $err,
+        parser => $parser,
+        line => $line,
+    };
+}
+
+sub compare_parse_events {
+    my ($self, $testcase, $result) = @_;
+    my $results = $self->{stats};
+    my $id = $testcase->{id};
+    my $title = $testcase->{title};
+    my $err = $result->{err};
+    my $yaml = $testcase->{in_yaml};
+    my $test_events = $testcase->{test_events};
+    my $exp_lines = () = $yaml =~ m/[\r\n]/g;
+
+    my @events = @{ $result->{events} };
+    $_ = encode_utf8 $_ for @events;
+
+    my $ok = 0;
+    if ($err) {
+        $results->{ERROR}++;
+        ok(0, "$id - $title (ERROR)");
+    }
+    else {
+        $ok = is_deeply(\@events, $test_events, "$id - $title");
+    }
+    if ($ok) {
+        $results->{OK}++;
+        my $lines = $result->{line};
+        cmp_ok($lines, '==', $exp_lines, "$id - Line count $lines == $exp_lines");
+    }
+    else {
+        push @{ $results->{DIFFS} }, $id unless $err;
+        $results->{DIFF}++;
+        if ($testcase->{todo}) {
+            $results->{TODO}++;
+        }
+        if (not $testcase->{todo} or $ENV{YAML_PP_TRACE}) {
+            diag "YAML:\n$yaml" unless $testcase->{todo};
+            diag "EVENTS:\n" . join '', map { "$_\n" } @$test_events;
+            diag "GOT EVENTS:\n" . join '', map { "$_\n" } @events;
+        }
+    }
+}
+
 
 1;
