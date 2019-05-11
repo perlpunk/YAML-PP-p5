bb54e2e0e301f1da59af9243e69157591ffb126e
Tina Müller <cpan2@tinita.de>
2019-05-11 14:48:55 +0200

    Refactor Dumper/Representer
    Dumper is now talking directly to emitter

--- lib/YAML/PP/Dumper.pm
+++ lib/YAML/PP/Dumper.pm
@@ -4,11 +4,20 @@ package YAML::PP::Dumper;
 
 our $VERSION = '0.000'; # VERSION
 
-use Scalar::Util qw/ blessed /;
+use Scalar::Util qw/ blessed refaddr reftype /;
 use YAML::PP;
+use YAML::PP::Emitter;
 use YAML::PP::Representer;
 use YAML::PP::Writer;
 use YAML::PP::Writer::File;
+use YAML::PP::Common qw/
+    YAML_PLAIN_SCALAR_STYLE YAML_SINGLE_QUOTED_SCALAR_STYLE
+    YAML_DOUBLE_QUOTED_SCALAR_STYLE YAML_QUOTED_SCALAR_STYLE
+    YAML_ANY_SCALAR_STYLE
+    YAML_LITERAL_SCALAR_STYLE YAML_FOLDED_SCALAR_STYLE
+    YAML_FLOW_SEQUENCE_STYLE YAML_FLOW_MAPPING_STYLE
+    YAML_BLOCK_MAPPING_STYLE YAML_BLOCK_SEQUENCE_STYLE
+/;
 
 sub new {
     my ($class, %args) = @_;
@@ -26,28 +35,121 @@ sub new {
     my $self = bless {
         representer => YAML::PP::Representer->new(
             schema => $schema,
-            emitter => $emitter,
         ),
+        emitter => $emitter,
+        seen => {},
+        anchors => {},
+        anchor_num => 0,
     }, $class;
     return $self;
 }
 
+sub init {
+    my ($self) = @_;
+    $self->{seen} = {};
+    $self->{anchors} = {};
+    $self->{anchor_num} = 0;
+}
+
+sub emitter { return $_[0]->{emitter} }
 sub representer { return $_[0]->{representer} }
 sub set_representer { $_[0]->{representer} = $_[1] }
 
 sub dump {
     my ($self, @docs) = @_;
-    $self->representer->emitter->init;
-    $self->representer->dump(@docs);
-    my $output = $self->representer->emitter->writer->output;
-    $self->representer->emitter->finish;
+    $self->emitter->init;
+
+    $self->emitter->stream_start_event({});
+
+    for my $i (0 .. $#docs) {
+        $self->emitter->document_start_event({ implicit => 0 });
+        $self->init;
+        $self->check_references($docs[ $i ]);
+        $self->dump_node($docs[ $i ]);
+        $self->emitter->document_end_event({ implicit => 1 });
+    }
+
+    $self->emitter->stream_end_event({});
+
+    my $output = $self->emitter->writer->output;
+    $self->emitter->finish;
     return $output;
 }
+
+sub dump_node {
+    my ($self, $value) = @_;
+    my $node = {
+        value => $value,
+    };
+    if (ref $value) {
+
+        my $seen = $self->{seen};
+        my $refaddr = refaddr $value;
+        if ($seen->{ $refaddr } and $seen->{ $refaddr } > 1) {
+            my $anchor = $self->{anchors}->{ $refaddr };
+            unless (defined $anchor) {
+                my $num = ++$self->{anchor_num};
+                $self->{anchors}->{ $refaddr } = $num;
+                $node->{anchor} = $num;
+            }
+            else {
+                $node->{value} = $anchor;
+                $self->emit_node([ alias => $node ]);
+                return;
+            }
+
+        }
+    }
+    $node = $self->representer->represent_node($node);
+    $self->emit_node($node);
+}
+
+sub emit_node {
+    my ($self, $item) = @_;
+    my ($type, $node) = @$item;
+    if ($type eq 'alias') {
+        $self->emitter->alias_event({ value => $node->{value} });
+        return;
+    }
+    if ($type eq 'mapping') {
+        my $style = YAML_BLOCK_MAPPING_STYLE;
+        $self->emitter->mapping_start_event({
+            anchor => $node->{anchor},
+            style => $style,
+            tag => $node->{tag},
+        });
+        for (@{ $node->{items} }) {
+            $self->dump_node($_);
+        }
+        $self->emitter->mapping_end_event;
+        return;
+    }
+    if ($type eq 'sequence') {
+        my $style = YAML_BLOCK_SEQUENCE_STYLE;
+        $self->emitter->sequence_start_event({
+            anchor => $node->{anchor},
+            style => $style,
+            tag => $node->{tag},
+        });
+        for (@{ $node->{items} }) {
+            $self->dump_node($_);
+        }
+        $self->emitter->sequence_end_event;
+        return;
+    }
+    $self->emitter->scalar_event({
+        value => $node->{items}->[0],
+        style => $node->{style},
+        anchor => $node->{anchor},
+        tag => $node->{tag},
+    });
+}
+
 
 sub dump_string {
     my ($self, @docs) = @_;
     my $writer = YAML::PP::Writer->new;
-    $self->representer->emitter->set_writer($writer);
+    $self->emitter->set_writer($writer);
     my $output = $self->dump(@docs);
     return $output;
 }
@@ -55,9 +157,43 @@ sub dump_string {
 sub dump_file {
     my ($self, $file, @docs) = @_;
     my $writer = YAML::PP::Writer::File->new(output => $file);
-    $self->representer->emitter->set_writer($writer);
+    $self->emitter->set_writer($writer);
     my $output = $self->dump(@docs);
     return $output;
+}
+
+my %_reftypes = (
+    HASH => 1,
+    ARRAY => 1,
+    Regexp => 1,
+    REGEXP => 1,
+    CODE => 1,
+    SCALAR => 1,
+    REF => 1,
+);
+
+sub check_references {
+    my ($self, $doc) = @_;
+    my $reftype = reftype $doc or return;
+    my $seen = $self->{seen};
+    # check which references are used more than once
+    if (++$seen->{ refaddr $doc } > 1) {
+        # seen already
+        return;
+    }
+    unless ($_reftypes{ $reftype }) {
+        die sprintf "Reference %s not implemented",
+            $reftype;
+    }
+    if ($reftype eq 'HASH') {
+        $self->check_references($doc->{ $_ }) for keys %$doc;
+    }
+    elsif ($reftype eq 'ARRAY') {
+        $self->check_references($_) for @$doc;
+    }
+    elsif ($reftype eq 'REF') {
+        $self->check_references($$doc);
+    }
 }
 
 1;
--- lib/YAML/PP/Representer.pm
+++ lib/YAML/PP/Representer.pm
@@ -6,7 +6,6 @@ our $VERSION = '0.000'; # VERSION
 
 use Scalar::Util qw/ reftype blessed refaddr /;
 
-use YAML::PP::Emitter;
 use YAML::PP::Common qw/
     YAML_PLAIN_SCALAR_STYLE YAML_SINGLE_QUOTED_SCALAR_STYLE
     YAML_DOUBLE_QUOTED_SCALAR_STYLE YAML_QUOTED_SCALAR_STYLE
@@ -19,95 +18,26 @@ use B;
 
 sub new {
     my ($class, %args) = @_;
-    my $emitter = delete $args{emitter} || YAML::PP::Emitter->new;
     my $self = bless {
         schema => $args{schema},
-        refs => {},
-        seen => {},
-        emitter => $emitter,
     }, $class;
     return $self;
 }
 
-sub init {
-    my ($self) = @_;
-    $self->{refs} = {};
-    $self->{seen} = {};
-}
-
-sub emitter { return $_[0]->{emitter} }
-sub set_emitter { $_[0]->{emitter} = $_[1] }
 sub schema { return $_[0]->{schema} }
 
-sub dump {
-    my ($self, @docs) = @_;
-    $self->init;
-    $self->emitter->stream_start_event({});
-    if (@docs) {
-        $self->emitter->document_start_event({ implicit => 0 });
-        for my $i (0 .. $#docs) {
-            my $doc = $docs[ $i ];
-            $self->dump_document($doc);
-            if ($i < $#docs) {
-                $self->emitter->document_end_event({ implicit => 1 });
-                $self->emitter->document_start_event({ implicit => 0 });
-            }
-        }
-        $self->emitter->document_end_event({ implicit => 1 });
-    }
-    $self->emitter->stream_end_event({});
-    return 1;
-}
+sub represent_node {
+    my ($self, $node) = @_;
 
-sub dump_document {
-    my ($self, $doc) = @_;
-    $self->{refs} = {};
-    $self->{anchor_num} = 0;
-    $self->check_references($doc);
-    $self->dump_node($doc);
-}
+    $node->{reftype} = reftype($node->{value});
 
-sub dump_node {
-    my ($self, $value) = @_;
-
-    my $seen = $self->{seen};
-    my $anchor;
-    my $ref = ref $value;
-    if (ref $value) {
-
-        my $refaddr = refaddr $value;
-        if ($seen->{ $refaddr } and $seen->{ $refaddr } > 1) {
-            $anchor = $self->{refs}->{ $refaddr };
-            unless (defined $anchor) {
-                my $num = ++$self->{anchor_num};
-                $self->{refs}->{ $refaddr } = $num;
-                $anchor = $num;
-            }
-            else {
-                $self->emitter->alias_event({ value => $anchor });
-                return;
-            }
-
-        }
-    }
-
-    my $node = {
-        value => $value,
-        reftype => reftype($value),
-        items => undef,
-        tag => undef,
-        data => undef,
-        style => undef,
-    };
-
-    if (ref $value) {
+    if (ref $node->{value}) {
         $self->represent_noderef($node);
     }
     else {
-        $self->represent_node($node);
+        $self->represent_node_nonref($node);
     }
-    $node->{reftype} = reftype $node->{data};
-    $node->{reftype} = '' unless defined $node->{reftype};
+    $node->{reftype} = (reftype $node->{data}) || '';
 
     if ($node->{reftype} eq 'HASH' and my $tied = tied(%{ $node->{data} })) {
         my $representers = $self->schema->representers;
@@ -125,27 +55,13 @@ sub dump_node {
                 push @{ $node->{items} }, $key, $node->{data}->{ $key };
             }
         }
-        my $style = YAML_BLOCK_MAPPING_STYLE;
-        $self->emitter->mapping_start_event({
-            anchor => $anchor,
-            style => $style,
-            tag => $node->{tag},
-        });
-        $self->dump_node($_) for @{ $node->{items} };
-        $self->emitter->mapping_end_event;
+        return [ mapping => $node ];
     }
     elsif ($node->{reftype} eq 'ARRAY') {
         unless (defined $node->{items}) {
             @{ $node->{items} } = @{ $node->{data} };
         }
-        my $style = YAML_BLOCK_SEQUENCE_STYLE;
-        $self->emitter->sequence_start_event({
-            anchor => $anchor,
-            style => $style,
-            tag => $node->{tag},
-        });
-        $self->dump_node($_) for @{ $node->{items} };
-        $self->emitter->sequence_end_event;
+        return [ sequence => $node ];
     }
     elsif ($node->{reftype}) {
         die "Reftype $node->{reftype} not implemented";
@@ -154,16 +70,12 @@ sub dump_node {
         unless (defined $node->{items}) {
             $node->{items} = [$node->{data}];
         }
-        $self->emitter->scalar_event({
-            value => $node->{items}->[0],
-            style => $node->{style},
-            anchor => $anchor,
-            tag => $node->{tag},
-        });
+        return [ scalar => $node ];
     }
+
 }
 
-sub represent_node {
+sub represent_node_nonref {
     my ($self, $node) = @_;
     my $representers = $self->schema->representers;
 
@@ -238,40 +150,6 @@ sub represent_noderef {
     }
     $node->{data} = $node->{value};
 
-}
-
-my %_reftypes = (
-    HASH => 1,
-    ARRAY => 1,
-    Regexp => 1,
-    REGEXP => 1,
-    CODE => 1,
-    SCALAR => 1,
-    REF => 1,
-);
-
-sub check_references {
-    my ($self, $doc) = @_;
-    my $reftype = reftype $doc or return;
-    my $seen = $self->{seen};
-    # check which references are used more than once
-    if (++$seen->{ refaddr $doc } > 1) {
-        # seen already
-        return;
-    }
-    unless ($_reftypes{ $reftype }) {
-        die sprintf "Reference %s not implemented",
-            $reftype;
-    }
-    if ($reftype eq 'HASH') {
-        $self->check_references($doc->{ $_ }) for keys %$doc;
-    }
-    elsif ($reftype eq 'ARRAY') {
-        $self->check_references($_) for @$doc;
-    }
-    elsif ($reftype eq 'REF') {
-        $self->check_references($$doc);
-    }
 }
 
 1;
